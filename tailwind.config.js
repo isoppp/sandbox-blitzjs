8fdfc0da6f0c0f216e673941815bdbca99f6dcde
isoppp <prog@d2deck.com>
2020-11-03 22:31:16 +0900

    add tailwind-css-variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,34 @@ module.exports = {
   purge: ['{app,pages}/**/*.{js,jsx,ts,tsx}'],
   theme: {},
   variants: {},
-  plugins: [],
+  plugins: [
+    require('tailwind-css-variables')(
+      {
+        colors: 'color',
+        screens: '', // '',
+        fontFamily: '', // 'font',
+        fontSize: false, // 'text',
+        fontWeight: false, // 'font',
+        lineHeight: false, // 'leading',
+        letterSpacing: false, // 'tracking',
+        backgroundSize: false, // 'bg',
+        borderWidth: false, // 'border',
+        borderRadius: false, // 'rounded',
+        width: false, // 'w',
+        height: false, // 'h',
+        minWidth: false, // 'min-w',
+        minHeight: false, // 'min-h',
+        maxWidth: false, // 'max-w',
+        maxHeight: false, // 'max-h',
+        padding: false, // 'p',
+        margin: false, // 'm',
+        boxShadow: 'shadows', // 'shadows',
+        zIndex: false, // 'z',
+        opacity: false, // 'opacity',
+      },
+      {
+        // options
+      },
+    ),
+  ],
 }
