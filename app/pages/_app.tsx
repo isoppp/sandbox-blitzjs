06b1ab71c17b28b6dcbcaad508c65691ea7eabf7
isoppp <prog@d2deck.com>
2020-11-03 17:17:25 +0900

    setup tailwindcss using blitz install tailwind

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,7 +1,9 @@
-import { AppProps, ErrorComponent, useRouter } from "blitz"
-import { ErrorBoundary, FallbackProps } from "react-error-boundary"
-import { queryCache } from "react-query"
-import LoginForm from "app/auth/components/LoginForm"
+import { AppProps, ErrorComponent, useRouter } from 'blitz'
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary'
+import { queryCache } from 'react-query'
+import LoginForm from 'app/auth/components/LoginForm'
+
+import 'app/styles/index.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
@@ -23,21 +25,13 @@ export default function App({ Component, pageProps }: AppProps) {
 }
 
 function RootErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
-  if (error?.name === "AuthenticationError") {
+  if (error?.name === 'AuthenticationError') {
     return <LoginForm onSuccess={resetErrorBoundary} />
-  } else if (error?.name === "AuthorizationError") {
+  } else if (error?.name === 'AuthorizationError') {
     return (
-      <ErrorComponent
-        statusCode={(error as any).statusCode}
-        title="Sorry, you are not authorized to access this"
-      />
+      <ErrorComponent statusCode={(error as any).statusCode} title="Sorry, you are not authorized to access this" />
     )
   } else {
-    return (
-      <ErrorComponent
-        statusCode={(error as any)?.statusCode || 400}
-        title={error?.message || error?.name}
-      />
-    )
+    return <ErrorComponent statusCode={(error as any)?.statusCode || 400} title={error?.message || error?.name} />
   }
 }
